8481a73830b4f374bec90e880825109e4adad264
isse <isse.isse@hotmail.com>
2022-02-01 16:22:19 +0000

    fizzBuzz task shortend with ternary operators

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -1,19 +1,28 @@
 'use strict';
 
-let chosenNum = 10;
 
-for (let i = 1; i <= 100; i++) {
 
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log(`FizzBuzz ${i}`);
-  }
-  if (i % 3 == 0) {
-    console.log(`Fizz ${i}`);
-  }
-  else if (i % 5 == 0) {
-    console.log(`Buzz ${i}`);
-  }
-  else {
-    continue;
-  }
+// for (let i = 1; i <= 100; i++) {
+
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(`FizzBuzz ${i}`);
+//   }
+//   if (i % 3 == 0) {
+//     console.log(`Fizz ${i}`);
+//   }
+//   else if (i % 5 == 0) {
+//     console.log(`Buzz ${i}`);
+//   }
+//   else {
+//     continue;
+//   }
+//     }
+
+// fizBuzz shorten with ternary operators
+    for (let i = 1; i <= 100; i++) {
+
+      (i % 3 == 0 && i % 5 == 0) ? console.log(`FizzBuzz ${i}`):
+      (i % 3 == 0 ) ? console.log(`Fizz ${i}`):
+      (i % 5 == 0) ? console.log(`Buzz ${i}`): " "
+       
     }
