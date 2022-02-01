02f69d945c1b1ebdf6c3b5f22eb1c7cd62011d30
isse <isse.isse@hotmail.com>
2022-02-01 14:41:38 +0000

    JS tasks 4 added

--- js/index.js
+++ js/index.js
@@ -43,3 +43,84 @@ let totalLeftToPay = totalMoney - moneyPaidSoFar;
 console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalMoney - moneyPaidSoFar}`);
 //OR 
 console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalLeftToPay}`);
+
+console.log(" "); // space in my web console
+
+//  QA iteration exercise 1-5
+// 1)
+
+for (let aa = 100; aa <= 201; aa++) {
+   
+    console.log(aa);
+    
+}
+
+console.log(" "); // space in my web console
+
+// 2)
+
+for (let bb = 100; bb <= 201; bb++) {
+   
+   if (bb % 2 == 0) {
+       console.log("- multiple of 2");
+   }
+   else {
+       console.log(" * not a multiple of 2");
+   }
+    
+}
+
+console.log(" "); // space in my web console
+
+// or //(q4)
+
+let bbb = 100
+while (bbb <= 200) {
+    if (bbb % 2 == 0) {
+        console.log("- multiple of 2");
+    }
+    else {
+        console.log(" * not a multiple of 2");
+    }
+
+    bbb++; 
+}
+
+// q3)
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < nums.length; i++) {
+
+    for (let j = 0; j < nums.length; j++) {
+        console.log(nums[j]);;
+        
+    }
+    
+}
+
+
+//q4 (see above)
+
+
+//q5 (see above)
+
+let today = new Date(); 
+let day = today.getDay();
+switch (day) {
+  case 0:
+    console.log(`Sunday`);
+    break;
+  case 1:
+    console.log(`Monday`);
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log(`Weekday`);
+    break;
+    case 6:
+        console.log(`Saturday`);
+        break;
+  default:
+    console.log(`day not found`);
+    break;
+}
