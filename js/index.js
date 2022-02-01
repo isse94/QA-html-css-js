453d35292e3c80ef467490fe7ca0a7bf0f6a6bfa
isse <isse.isse@hotmail.com>
2022-02-01 10:41:17 +0000

    JS task 1 added

---
+++ js/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// ex1)
+let firstName = "Isse";
+let secondName = "Isse2";
+let fromLoc ="London";
+let stars = "Stars"
+
+console.log(firstName);
+console.log(secondName);
+console.log(fromLoc);
+console.log(stars);
+
+// ex2)
+let favCar = "BMW";
+let model = "X6 SERIES"
+
+console.log("my fav car is: " + favCar + " and the model is " + model);
+
+//ex3)
+console.log(` %c my fav car is: ${favCar} and the model is  ${model}` ,"color: orange;font-family:fantasy; font-style: Bold; background-color: black; padding: 10px");
