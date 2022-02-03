8bc060f9e34c37176d02a474bd6fcc33b0a927bf
isse <isse.isse@hotmail.com>
2022-02-03 10:05:02 +0000

    day3 calculator task finished with JS added

--- js/calculatorProj/cal.js
+++ js/calculatorProj/cal.js
@@ -14,11 +14,24 @@ console.log(DOM.buttonsJS.innerText="hello");
 //// apply a click event listener to every button in our array
 DOM.buttonsJS.map( button => {
     button.addEventListener('click', (e) => {
-    console.log('clicked');
-    console.log(e);// indentifies which button was clicked
-    console.log(e.target);
-    console.log(e.target.innerText); // take different actions based on it
-    
+    // console.log('clicked');
+    // console.log(e);// indentifies which button was clicked
+    // console.log(e.target);
+    // console.log(e.target.innerText); // take different actions based on it
+    switch (e.target.innerText) {
+        case 'C':
+            DOM.display.innerText = ' ';
+            break;
+            case '←':
+                DOM.display.innerText = DOM.display.innerText.slice(0, -1); //remobe our last value of array using slice from last (-1) to index (0)
+                break;
+            case '=':
+                DOM.display.innerText = eval(DOM.display.innerText );  // eval takes our string (our display text) as parameter and executes it as a js code
+                break; // (stop the equal sign from displaying)
+                // can add try{} and catch { display error message as text} to show user they've inputed an error of some sort
+                default:
+            DOM.display.innerText += e.target.innerText; // display what is clicked in our cal output
+    }
     
     });
     });
--- js/calculatorProj/calDom.js
+++ js/calculatorProj/calDom.js
@@ -5,7 +5,7 @@ export const display = document.querySelector('#display');
 // export const buttons = document.querySelector('.buttons');
 //^ (collection of html elements) indstead use. to convert out html to an array 
 // therefore displays
-export const buttonsJS = Array.from(document.querySelector('.button'));
+export const buttonsJS = Array.from(document.getElementsByClassName('button'));
 // export const button = document.querySelector('.button');
 
 
